ef35ce1fab8e085b73014f699087180ff8c480fe
anakha <anakha@veuz.in>
2024-03-22 14:57:04 +0530

    career

---
+++ header.php
@@ -0,0 +1,39 @@
+<div class="header">
+        <nav>
+            <div class="navbar">
+                <i class='bx bx-menu'></i>
+                <div class="logo">
+                    <a href="index.php"><img src="img/logo.png" alt=""></a>
+                </div>
+                <div class="nav-links">
+                    <div class="sidebar-logo">
+                        <span class="logo-name"></span>
+                        <i class='bx bx-x'></i>
+                    </div>
+                    <ul class="links">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="service.php">Solutions</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">What we do</a></li>
+                        <li><a href="blog.php">Insight</a></li>
+                        <li><a href="career.php">Careers</a></li>
+                        <li><a href="contact.php">Contact us</a></li>
+                    </ul>
+                    <div class="lang">
+                        <a href="#">
+                            <h6>Eng</h6>
+                            <img src="img/eng.png" alt="">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="lang d-sm-none d-block lang-mob">
+                <a href="#">
+                    <h6>Eng</h6>
+                    <img src="img/eng.png" alt="">
+                </a>
+            </div>
+        </nav>
+    </div>
